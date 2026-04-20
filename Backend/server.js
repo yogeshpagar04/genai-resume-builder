5731c9a99e74328557e4e66386eb8728ad3730c9
yogesh pagar <yogeshpagar1661@gmail.com>
2026-04-20 22:34:16 +0530

    backend for genai resume builder

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,21 @@
-// require("dotenv").config()
-// const app = require("./src/app")
-// const connectToDB = require("./src/config/database")
-
-// connectToDB()
-
-
-// app.listen(3000, () => {
-//     console.log("Server is running on port 3000")
-// })
 require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/database")
 
 connectToDB()
 
-const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
+// require("dotenv").config()
+// const app = require("./src/app")
+// const connectToDB = require("./src/config/database")
+
+// connectToDB()
+
+// const PORT = process.env.PORT || 3000
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`)
+// })
